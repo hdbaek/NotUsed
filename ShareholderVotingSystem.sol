2190ab4626188a946c7bcbaeec2ad16c1997605e
BAEK <39151271+hdbaek@users.noreply.github.com>
2018-05-29 11:04:55 +0900

    Update ShareholderVotingSystem.sol
    Added functions:
    1. stockholder can entrust their voting shares to others
    2. In the "struct Agenda", "startTime" added
    3. mapping data "voteShares" added to save one's voting shares

--- ShareholderVotingSystem.sol
+++ ShareholderVotingSystem.sol
@@ -47,7 +47,8 @@ library SafeMath {
 }
 
 /*
-*
+*   @title  CompanyShares
+*   @dev    Issue, buy, sell and transfer of stocks in a company
 */
 contract CompanyShares {
     using SafeMath for uint256;
@@ -64,6 +65,10 @@ contract CompanyShares {
         require(msg.sender == owner);
         _;
     }
+    /*
+    *   @dev    When deploy, this function is executed. 
+    *           In real world, startup a company.
+    */
     constructor(uint256 supply, uint _price) public {
         shareholders.push(msg.sender);
         shareholderToShares[msg.sender] = supply;
@@ -76,22 +81,34 @@ contract CompanyShares {
     function getShareByAddress(address _address) view public returns(uint256) {
         return shareholderToShares[_address];
     }
+    /*  @dev    owner may deposit some for refund (maybe ?)
+    */
     function deposit() public payable onlyOwner {
         balance = msg.value;
     }
     function getBalance() view public returns(uint256) {
         return balance;
     }
+    /*
+    *   @dev    Anybody can by the stocks from the original owner
+    */
     function buyShares(uint256 amount) public payable {
         require(shareholderToShares[owner] >= amount);
         require(msg.value >= amount.mul(sharePrice));
         //balance = balance.add(amount*sharePrice);
         owner.transfer(amount.mul(sharePrice));
         shareholderToShares[owner] = shareholderToShares[owner].sub(amount);
-        shareholders.push(msg.sender); //check duplicate
+         
+        //check if there is duplicate address
+        for (uint i = 0; i < shareholders.length; i++)
+            if (shareholders[i] == msg.sender) break;
+        if (i == shareholders.length) shareholders.push(msg.sender);
+        
         shareholderToShares[msg.sender] = 
             shareholderToShares[msg.sender].add(amount);
     }
+    /*  @dev The owner only can approve the withdraw of purchasing stocks
+    */
     function approve(address _to, bool yesNo) public onlyOwner {
         approvals[_to] = yesNo;
     } 
@@ -108,6 +125,10 @@ contract CompanyShares {
     function changePriceOfShare(uint _price) public onlyOwner {
         sharePrice = _price;
     }
+    /*
+    *   @dev    shareholders can transfer their stocks to others
+    *           without any condition
+    */
     function transferShares(address _to, uint256 shares) public {
         require(shareholderToShares[msg.sender] >= shares);
         shareholderToShares[_to] = shares;
@@ -115,18 +136,26 @@ contract CompanyShares {
     }
 }
 /*
-*
+*   @title  VotingOnAgenda
+*   @dev    Voting system to handle a agneder suggested in a stockholder's meeting
 */
-contract Voting is CompanyShares {
-    using SafeMath for uint32;
+contract VotingOnAgenda is CompanyShares {
+    // to prevent some possible problems in handling integer data
+    using SafeMath for uint32;  
+    using SafeMath for uint256; 
+     
+    event AgendaSetup(string agenda, uint256 startTime, uint256 endTime);
+    event AgendaVote(address voter, uint256 votingTime, uint256 sharesToVote);
     
-    mapping (address => uint32) shareholderToVotingCount;
-    
-    struct AgendaForVoting {
+    mapping (address => uint256) shareholderToVotingCount;
+    mapping (address => uint256) votingShares;
+     
+    struct Agenda {
         string contents;
+        uint256 startTime;
         uint256 endTime;
     }
-    AgendaForVoting agendaForVoting;
+    Agenda agendaForVoting;
     struct AgendaVoting {
         string  choice;
         uint32  votes;
@@ -134,6 +163,7 @@ contract Voting is CompanyShares {
     AgendaVoting[] agendaVotings;
     
     modifier withinDeadLine () {
+        require(agendaForVoting.startTime <= now);
         require(agendaForVoting.endTime >= now);
         _;
     }
@@ -144,22 +174,49 @@ contract Voting is CompanyShares {
         require (i < shareholders.length);
         _;
     }
-    function addAgendaContents(string _agendaContents, uint _endTime) public onlyOwner {
+    /*
+    *   @dev Starting the processing of a agenda, set up inital data
+    */
+    function setupAgenda(string _agendaContents, uint256 _startTime, 
+                        uint256 _endTime) public onlyOwner {
          agendaForVoting.contents = _agendaContents;
+         agendaForVoting.startTime = _startTime;
          agendaForVoting.endTime = _endTime;
+         
+         // Copy from shareholderToShares to votingShares
+         // Initially, the number of shares equals to the number of voting shares
+         for (uint i = 0; i < shareholders.length; i++) {
+             votingShares[shareholders[i]] = shareholderToShares[shareholders[i]];
+         }
+         emit AgendaSetup(_agendaContents, _startTime, _endTime);
     }
     function addAgendaVoting(string _choice) public onlyOwner {
         agendaVotings.push(AgendaVoting(_choice, 0));
     }
-    function getAgendaVotingResult(uint index) view public returns (uint32) {
+    
+    /*
+    *  @dev Below functions are for only viewing the current status
+    */
+    function getAgendaVotingVotes(uint index) view public returns (uint32) {
         return agendaVotings[index].votes;
     }
-    function getAgenda() public view returns(string) {
+    function getAgendaVotingChoice(uint index) view public returns(string) {
+        return agendaVotings[index].choice;
+    }
+    function getAgendaContents() public view returns(string) {
         return agendaForVoting.contents;
+    }
+    function getStartTime() public view returns(uint256) {
+        return agendaForVoting.startTime;
     }
     function getEndTime() public view returns(uint256) {
         return agendaForVoting.endTime;
     }
+    
+    /*
+    *  @dev Anybody can participate in the voting if he or she has some 
+    *       voting shares
+    */
     function vote(uint32 index, uint32 sharesToVote) public onlyShareholders withinDeadLine {
         require(index >= 0);
         require(index < agendaVotings.length);
@@ -167,5 +224,15 @@ contract Voting is CompanyShares {
                 <= shareholderToShares[msg.sender]);
         agendaVotings[index].votes++;
         shareholderToVotingCount[msg.sender].add(sharesToVote);
+        emit AgendaVote(msg.sender, now, sharesToVote);
+    }
+    /*
+    *  @dev Shareholders can yield their voting shares to other address
+    *       only between startTime  and endTime
+    */
+    function transferVotingShares(address _to, uint256 shares) public withinDeadLine {
+        require(votingShares[msg.sender] >= shares);
+        votingShares[_to] = shares;
+        votingShares[msg.sender] = votingShares[msg.sender].sub(shares);
     }
 }
